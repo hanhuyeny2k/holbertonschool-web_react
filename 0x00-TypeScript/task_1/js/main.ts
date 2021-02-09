d34fd93173aafba063ac8252df02ae6eb9a108ba
Hanh Nguyen <hanhuyeny2k@gmail.com>
2021-02-09 04:17:14 +0000

    create a Class named StudentClass

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -32,3 +32,18 @@ const director1: Directors = {
   numberOfReports: 17,
 };
 console.log(director1);
+
+// returns the first letter of the firstName and the full lastName
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`;
+}
+
+console.log(printTeacher('John', 'Doe'));
+
+// class description interface
+interface classInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
