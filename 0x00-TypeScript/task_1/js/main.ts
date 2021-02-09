a2107b5db331c1215aaf3fb7d86c5a6373e3df73
Hanh Nguyen <hanhuyeny2k@gmail.com>
2021-02-09 04:21:37 +0000

    create workOnHomework and displayName class

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -47,3 +47,33 @@ interface classInterface {
   workOnHomework(): string;
   displayName(): string;
 }
+
+// constructor description interface
+interface classConstructor {
+  new (firstName: string, lastName: string): classInterface;
+}
+
+// Creating class and constructor through interfaces 
+class StudentClass implements classInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+// creating instance of StudentClass
+const studentClass: StudentClass = new StudentClass('bobby', 'lee');
+console.log(studentClass.displayName());
+console.log(studentClass.workOnHomework());
+
