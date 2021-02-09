0f4dbeaa8fe9283d3b802cb9dea7bb17e88bfa79
Hanh Nguyen <hanhuyeny2k@gmail.com>
2021-02-09 04:09:39 +0000

    extending the teacher class using Directors interface

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -8,3 +8,27 @@ interface Teacher {
   yearsOfExperience: number;
   [propName: string]: any;
 }
+
+// Directors interface
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Creating a teacher and a director
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+console.log(teacher3);
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
