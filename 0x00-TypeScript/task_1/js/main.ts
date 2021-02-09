cbce8b7dc9ed331f03c36d4d49ad9f56b2037abd
Hanh Nguyen <hanhuyeny2k@gmail.com>
2021-02-09 04:04:41 +0000

    build Teacher interface

---
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -0,0 +1,10 @@
+// Teacher interface
+
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  location: string;
+  yearsOfExperience: number;
+  [propName: string]: any;
+}
