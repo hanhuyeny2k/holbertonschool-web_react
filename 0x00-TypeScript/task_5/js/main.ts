dd79392729dab2a81dc1248e579a8135a2f7e904
Hanh Nguyen <hanhuyeny2k@gmail.com>
2021-02-10 05:44:33 +0000

    brand convention and nominal typing

---
+++ 0x00-TypeScript/task_5/js/main.ts
@@ -0,0 +1,25 @@
+interface MajorCredits {
+  credits: number;
+  _majorBrand: 'major';
+}
+
+interface MinorCredits {
+  credits: number;
+  _minorBrand: 'minor';
+}
+
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): number {
+  return subject1.credits + subject2.credits;
+}
+
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): number {
+  return subject1.credits + subject2.credits;
+}
+
+const Accounting = { credits: 3 } as MajorCredits;
+const Economic = { credits: 3 } as MajorCredits;
+const Music = { credits: 1 } as MinorCredits; 
+const Theater = { credits: 1 } as MinorCredits; 
+
+console.log(sumMajorCredits(Accounting, Economic));
+console.log(sumMinorCredits(Music, Theater));
