18156a83559ce32e963510254786c5806c228b2d
Hanh Nguyen <hanhuyeny2k@gmail.com>
2021-02-16 02:03:25 +0000

    correct syntax error

--- 0x01-Webpack/task_2/js/dashboard_main.js
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -1,11 +1,12 @@
 import $ from 'jquery';
 import _ from 'lodash';
+import '../css/main/css';
 
 $(document).ready(function() {
   $('body').append('<p>Holberton Dashboard</p>');
   $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append(`<button>Click here to get started</button>`);
-  $('body').append(`<p id='count'></p>`);
+  $('body').append('<button>Click here to get started</button>');
+  $('body').append('<p id="count"></p>');
   $('body').append('<p>Copyright - Holberton School</p>');
 });
 
