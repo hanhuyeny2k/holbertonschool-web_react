476bacbb2d73506e861a889c1c0a80091270c25f
Hanh Nguyen <hanhuyeny2k@gmail.com>
2021-02-16 03:46:59 +0000

    change X to 1 for student

--- 0x00-TypeScript/task_0/js/main.ts
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -5,21 +5,21 @@ interface Student {
 	location: string;
 }
 
-const studentX: Student = {
+const student1: Student = {
 	firstName: 'Mellisa',
 	lastName: 'Johnson',
 	age: 38,
 	location: 'California'
 }
 
-const studentY: Student = {
+const student2: Student = {
 	firstName: 'Misty',
 	lastName: 'Lane',
 	age: 39,
 	location: 'Panama',
 }
 
-const studentList = [studentX, studentY];
+const studentList = [student1, student2];
 
 const table = document.createElement('table');
 const tbody = document.createElement('tbody');
