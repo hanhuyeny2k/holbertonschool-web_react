8b2a5fe15ab7ba0529ab3823266f136d83863051
Hanh Nguyen <hanhuyeny2k@gmail.com>
2021-02-16 03:25:55 +0000

    updated syntax error

--- 0x01-Webpack/task_2/js/dashboard_main.js
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -2,24 +2,17 @@ import $ from 'jquery';
 import _ from 'lodash';
 import '../css/main/css';
 
-$(document).ready(function() {
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button>Click here to get started</button>');
-  $('body').append('<p id="count"></p>');
-  $('body').append('<p>Copyright - Holberton School</p>');
-});
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
 
 // increments and renders a counter for each time a button is clicked
 function updateCounter() {
-  $(document).ready(function() {
-    let counter = $('#count').html()
-    $('button').click(function() {
-        counter++;
-        $('#count').html(`${counter} clicks on the button`);
-    });
-  });
-}
+  let counter = 0;
+  counter++;
+  $('#count').html(`${counter} clicks on the button`);
+};
 
-_.debounce(updateCounter, 250);
-updateCounter();
+$('button').on('click', _.debounce(updateCounter));
