6ce6325306a6dcd3f13ace16f1a5a2eb1437f7bc
Hanh Nguyen <hanhuyeny2k@gmail.com>
2021-02-20 01:42:44 +0000

    modify the notifications

--- 0x02-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x02-react_intro/task_2/dashboard/src/Notifications.js
@@ -5,6 +5,25 @@ export const Notification = () => {
   return (
     <div className='Notifications'>
       <p>Here is the list of notifications</p>
+      <ul>
+        <li data-priority='default'>New course available</li>
+        <li data-priority='urgent'>New resume available</li>
+        <li
+          data-priority='urgent'
+          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+        ></li>
+      </ul>
+      <button
+        className='close-icon'
+        aria-label='Close'
+        onClick={() => console.log('Close button has been clicked')}
+      >
+        <img
+          src={closeIcon}
+          alt='Close'
+          style={{ height: '20px', width: '20px' }}
+        />
+      </button>
     </div>
   );
 };
